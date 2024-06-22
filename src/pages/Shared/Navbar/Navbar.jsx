942bb462e230fa4dbb8229244f1c0a4bcd68dc9f
Sabbir Hossain <sabbirh8181@gmail.com>
2024-06-23 01:40:19 +0600

    nav updates

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -91,7 +91,7 @@ const NavBar = () => {
   return (
     <div>
         <Navbar className="fixed z-10 max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4 bg-white dark:bg-primary-dark mx-auto">
-          <div className="flex items-center justify-between">
+          <div className="flex items-center justify-between text-nowrap">
             <Typography
               as="a"
               href="#"
