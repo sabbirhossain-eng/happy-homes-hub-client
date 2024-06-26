b2f95e21e8ef5dfbc8d807ada3934f5a09e74b14
Sabbir Hossain <sabbirh8181@gmail.com>
2024-06-26 16:07:55 +0600

    loading page updates

--- src/Routes/AdminRoute.jsx
+++ src/Routes/AdminRoute.jsx
@@ -1,7 +1,6 @@
 import { Navigate, useLocation } from "react-router-dom";
 import useAdmin from "../Hooks/useAdmin";
 import useAuth from "../Hooks/useAuth";
-import { Spinner } from "@material-tailwind/react";
 
 const AdminRoute = ({children}) => {
     const {user, loading} = useAuth()
@@ -9,7 +8,28 @@ const AdminRoute = ({children}) => {
     const location = useLocation();
 
     if(loading || isAdminLoading){
-       return <Spinner className="h-16 w-16" color="amber" />
+        return (
+            <div
+              aria-label="Loading..."
+              role="status"
+              className="flex items-center space-x-2 justify-center mt-16"
+            >
+              <svg
+                className="h-20 w-20 animate-spin stroke-primary-light"
+                viewBox="0 0 256 256"
+              >
+                <line x1="128" y1="32" x2="128" y2="64"></line>
+                <line x1="195.9" y1="60.1" x2="173.3" y2="82.7"></line>
+                <line x1="224" y1="128" x2="192" y2="128"></line>
+                <line x1="195.9" y1="195.9" x2="173.3" y2="173.3"></line>
+                <line x1="128" y1="224" x2="128" y2="192"></line>
+                <line x1="60.1" y1="195.9" x2="82.7" y2="173.3"></line>
+                <line x1="32" y1="128" x2="64" y2="128"></line>
+                <line x1="60.1" y1="60.1" x2="82.7" y2="82.7"></line>
+              </svg>
+              <span className="text-4xl font-medium text-primary-light">Loading...</span>
+            </div>
+          );
     }
     if(user && isAdmin){
         return children
--- src/Routes/PrivetRoute.jsx
+++ src/Routes/PrivetRoute.jsx
@@ -1,18 +1,38 @@
 import { Navigate, useLocation } from "react-router-dom";
 import useAuth from "../Hooks/useAuth";
-import { Spinner } from "@material-tailwind/react";
 
-const PrivetRoute = ({children}) => {
-    const {user, loading} = useAuth();
-    const location = useLocation();
+const PrivetRoute = ({ children }) => {
+  const { user, loading } = useAuth();
+  const location = useLocation();
 
-    if(loading){
-       return <Spinner className="h-16 w-16" color="amber" />
-    }
-    if(user){
-        return children
-    }
-    return <Navigate state={{from: location}} replace to="/login"/>
+  if (loading) {
+    return (
+      <div
+        aria-label="Loading..."
+        role="status"
+        className="flex items-center space-x-2 justify-center mt-16"
+      >
+        <svg
+          className="h-20 w-20 animate-spin stroke-primary-light"
+          viewBox="0 0 256 256"
+        >
+          <line x1="128" y1="32" x2="128" y2="64"></line>
+          <line x1="195.9" y1="60.1" x2="173.3" y2="82.7"></line>
+          <line x1="224" y1="128" x2="192" y2="128"></line>
+          <line x1="195.9" y1="195.9" x2="173.3" y2="173.3"></line>
+          <line x1="128" y1="224" x2="128" y2="192"></line>
+          <line x1="60.1" y1="195.9" x2="82.7" y2="173.3"></line>
+          <line x1="32" y1="128" x2="64" y2="128"></line>
+          <line x1="60.1" y1="60.1" x2="82.7" y2="82.7"></line>
+        </svg>
+        <span className="text-4xl font-medium text-primary-light">Loading...</span>
+      </div>
+    );
+  }
+  if (user) {
+    return children;
+  }
+  return <Navigate state={{ from: location }} replace to="/login" />;
 };
 
 export default PrivetRoute;
